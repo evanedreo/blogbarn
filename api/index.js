605529d436dd142c3978e593d79559dd89329a17
Evan Honggo Widjojo <evanedreo@gmail.com>
2024-01-06 02:42:56 -0800

    implement login and logout functionality and also added UserContext

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const User = require("./models/User");
 const app = express();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "randomstring";
 
-app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect(
   "mongodb+srv://evanedreo:msnzj4F5MDYQCyaa@cluster0.tpmdgsu.mongodb.net/?retryWrites=true&w=majority"
@@ -36,12 +38,27 @@ app.post("/login", async (req, res) => {
   if (passOk) {
     jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
       if (err) throw err;
-      res.cookie('token',token).json('ok')
+      res.cookie("token", token).json({
+        id:userDoc._id,
+        username,
+      });
     });
   } else {
     res.status(400).json("wrong credentials");
   }
 });
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post("/logout", (req,res)=>{
+  res.cookie('token','').json('ok')
+})
 
 app.listen(4000);
 
--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,11 @@ import { Route, Routes } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import { UserContextProvider } from "./UserContext";
 
 function App() {
   return (
+    <UserContextProvider>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
@@ -16,6 +18,7 @@ function App() {
         <Route path="/register" element={<RegisterPage/>}/>
       </Route>
     </Routes>
+    </UserContextProvider>
   );
 }
 
